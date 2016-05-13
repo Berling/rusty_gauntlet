29a960e244fdd83a2552c1dbfbbe5c9a4544aab6
Georg Schäfer <georg.peter.schaefer@gmail.com>
2016-05-13 20:02:52 +0200

    Add main loop.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+#[macro_use]
+extern crate glium;
+
 fn main() {
-    println!("Hello World!");
+    use glium::{DisplayBuild, Surface};
+    let display = glium::glutin::WindowBuilder::new()
+        .with_dimensions(800, 600)
+        .with_title(format!("Rusty Gauntlet™"))
+        .build_glium()
+        .unwrap();
+
+    loop {
+        let mut target = display.draw();
+        target.clear_color(0.5, 0.6, 0.9, 1.0);
+        target.finish().unwrap();
+
+        for ev in display.poll_events() {
+            match ev {
+                glium::glutin::Event::Closed => return,
+                _ => ()
+            }
+        }
+    }
 }
