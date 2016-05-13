7dce6f5dc446b3cc3224fccbc01741182a735ffc
Thomas Eppers <epperst@hochschule-trier.de>
2016-05-14 00:24:30 +0200

    refactored the move logic to a different function

--- src/input.rs
+++ src/input.rs
@@ -5,6 +5,13 @@ pub mod input {
     use input::glium::glutin::Event;
 
     pub struct Input;
+
+    pub enum Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT,
+    }
     
     impl Input {
 
@@ -13,16 +20,23 @@ pub mod input {
         }
         
         pub fn process_input(&self, event:Event) -> bool {
-
-            use input::glium::glutin::{ElementState, VirtualKeyCode as KeyCode};
+            use input::glium::glutin::Event::KeyboardInput as Keyboard;
+            use input::glium::glutin::{ElementState as State, VirtualKeyCode as KeyCode};
             match event {
                 Event::Closed => true,
-                Event::KeyboardInput(ElementState::Pressed, _, Some(KeyCode::Escape)) => true,
-                
+                Keyboard(State::Pressed, _, Some(KeyCode::Escape)) => true,
+                Keyboard(State::Pressed, _, Some(KeyCode::Up)) => self.move_dir(Direction::UP),
+                Keyboard(State::Pressed, _, Some(KeyCode::Down)) => self.move_dir(Direction::DOWN),
+                Keyboard(State::Pressed, _, Some(KeyCode::Left)) => self.move_dir(Direction::LEFT),
+                Keyboard(State::Pressed, _, Some(KeyCode::Right)) => self.move_dir(Direction::RIGHT),
+                Keyboard(State::Pressed, _, Some(KeyCode::Return)) => false,
                 _ => false,
             }
         }
 
+        pub fn move_dir(&self, dir:Direction) -> bool{
 
+            false
+        }
     }
 }
